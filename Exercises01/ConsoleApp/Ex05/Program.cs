3d309748cc59b56afd11ce8113b7571d4f7a5d5c
Tomas Prudky <64365934+st55790@users.noreply.github.com>
2021-02-21 14:17:39 +0100

    ex05 60%

--- Exercises01/ConsoleApp/Ex05/Program.cs
+++ Exercises01/ConsoleApp/Ex05/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Xml.Linq;
+using System.IO;
+using System.Net;
 
 namespace Ex05
 {
@@ -9,17 +8,47 @@ namespace Ex05
     {
         static void Main(string[] args)
         {
+            string localDate = Round(DateTime.Now).ToString("yyyy.MM.dd HH:mm:ss");
+            localDate = localDate.Replace('.', '-');
+            localDate = localDate.Replace(' ', 'T');
+            localDate = localDate.Insert(localDate.Length, "Z");
+            //Console.WriteLine("Today is: " + localDate);
+
             System.Net.WebClient webClient = new System.Net.WebClient();
             string raw = webClient.DownloadString("https://api.met.no/weatherapi/locationforecast/1.9/?lat=49.50&lon=16.59&msl=70");
-           
-            Console.WriteLine(raw);
 
-            var str = XElement.Parse(raw);
+            //Console.WriteLine(raw);
+            string pattern = @$"<time datatype=""forecast"" from=""{localDate}"" to=""{localDate}"">(.*?)</time>";
+            string p = "<time datatype=\"forecast\" from=\"" + localDate +"\" to=\"" + localDate + "\">(.*?)</time>";
+            string pat1 = @$"<time datatype=""forecast"" from=""{localDate}"" to=""{localDate}"">";
+            string pat2 = "</time>";
 
+            string actualWeather = GetBetween(raw, pat1, pat2);
+            string temperature = GetBetween(actualWeather, "<temperature id=\"TTT\" unit=\"celsius\" value=\"", "\"");
+            string windspeed = GetBetween(actualWeather, "<windSpeed id=\"ff\" mps=\"", "\"");
+            string presure = GetBetween(actualWeather, "<pressure id=\"pr\" unit=\"hPa\" value=\"", "\"");
+            Console.WriteLine(temperature + " Celsium");
+            Console.WriteLine(windspeed + " mps");
+            Console.WriteLine(presure + " hPa");
+        }
 
-            //var result = str.Elements("time").Where(x=>x.Element(""))
+        public static DateTime Round(DateTime dateTime)
+        {
+            var updated = dateTime.AddMinutes(30);
+            return new DateTime(updated.Year, updated.Month, updated.Day, updated.Hour, 0, 0, dateTime.Kind);
+        }
 
-
+        public static string GetBetween(string content, string startString, string endString)
+        {
+            int Start = 0, End = 0;
+            if (content.Contains(startString) && content.Contains(endString))
+            {
+                Start = content.IndexOf(startString, 0) + startString.Length;
+                End = content.IndexOf(endString, Start);
+                return content.Substring(Start, End - Start);
+            }
+            else
+                return string.Empty;
         }
     }
 }
