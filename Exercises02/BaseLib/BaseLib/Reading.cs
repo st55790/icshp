53eac43d90b98da812b33d48bbb359769c6bccad
Tomas Prudky <64365934+st55790@users.noreply.github.com>
2021-02-24 20:51:39 +0100

    commit ex02

---
+++ Exercises02/BaseLib/BaseLib/Reading.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fei.BaseLib
+{
+    public class Reading
+    {
+
+        /// <summary>
+        /// Method print message to screen and parse user input to double.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>user input as double</returns>
+
+        public static double ReadDouble(string message)
+        {
+            Console.Write($"{message}: ");
+
+            if (double.TryParse(Console.ReadLine(), out double parsedValue))
+            {
+                return parsedValue;
+            }
+
+            return double.NaN;
+        }
+
+        /// <summary>
+        /// Method print message to screen and parse user input to int.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>user input as int</returns>
+
+        public static int ReadInt(string message)
+        {
+            Console.Write($"{message}: ");
+
+            if (int.TryParse(Console.ReadLine(), out int parsedValue))
+            {
+                return parsedValue;
+            }
+
+            return int.MinValue;
+        }
+
+        /// <summary>
+        /// Method print message to screen and parse user input to char.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>user input as char</returns>
+
+        public static char ReadChar(string message)
+        {
+            Console.Write($"{message}: ");
+            string userInput = Console.ReadLine();
+            if (!string.IsNullOrEmpty(userInput))
+            {
+                return userInput[0];
+            }
+            throw new InvalidCastException("String was empty or null");
+
+        }
+
+        /// <summary>
+        /// Method print message to screen and parse user input to string.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>user input as string</returns>
+
+        public static string ReadString(string message)
+        {
+            Console.Write($"{message}: ");
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ Exercises02/BaseLib/DemoApp/Program.cs
@@ -0,0 +1,131 @@
+﻿using Fei.BaseLib;
+using System;
+using System.Linq;
+
+namespace DemoApp
+{
+    class Program
+    {
+        public static int[] array;
+        static void Main(string[] args)
+        {
+            ShowMenu();
+            Console.ReadKey();
+        }
+
+        public static void ShowMenu() {
+            bool repeat = true;
+
+            while (repeat) {
+
+                Console.WriteLine("1.Zadaní prvků pole z klávesnice\n" +
+                                  "2.Výpis pole na obrazovku\n" +
+                                  "3.Utřídění pole vzestupně\n" +
+                                  "4.Utřídění pole sestupně\n" +
+                                  "5.Hledání minimálního prvku\n" +
+                                  "6.Hledání prvního výskytu zadaného čísla\n" +
+                                  "7.Hledání posledního výskytu zadaného čísla\n" +
+                                  "8.Konec programu");
+                string choice = Reading.ReadString("Zadej volbu");
+                switch (choice)
+                {
+                    case "1":
+                        ReadFromKeyboard();
+                        break;
+                    case "2":
+                        PrintArray();
+                        break;
+                    case "3":
+                        SortAsc();
+                        break;
+                    case "4":
+                        SortDesc();
+                        break;
+                    case "5":
+                        GetMin();
+                        break;
+                    case "6":
+                        GetFirstOccurenceInArray();
+                        break;
+                    case "7":
+                        GetLastOccurenceInArray();
+                        break;
+                    case "8":
+                        repeat = false;
+                        Console.WriteLine("Konec programu");
+                        break;
+                    default:
+                        Console.WriteLine("Neplatná volba!\nLze zadat pouze 1-8\n");
+                        break;
+                }
+            }
+        }
+
+        private static void SortDesc()
+        {
+            array = array.OrderByDescending(x => x).ToArray();
+        }
+
+        private static void GetLastOccurenceInArray()
+        {
+            int choice = Reading.ReadInt("Zadej hledané číslo");
+            int index = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == choice)
+                {
+                    index = i;
+                }
+            }
+
+            if (index != -1)
+            {
+                Console.WriteLine("První výskyt čísla " + choice + " je na indexu: " + index + "\n");
+            }
+            else { 
+                Console.WriteLine("Hledané číslo není v poli");
+            }
+        }
+
+        private static void GetFirstOccurenceInArray()
+        {
+            int choice = Reading.ReadInt("Zadej hledané číslo");
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] == choice) {
+                    Console.WriteLine("První výskyt čísla " + choice + " je na indexu: " + i + "\n");
+                    break;
+                }
+            }
+            Console.WriteLine("Hledané číslo není v poli");
+        }
+
+        private static void GetMin()
+        {
+            Console.WriteLine("Nejmenší prvek je: " + array.Min());
+        }
+
+        private static void SortAsc()
+        {
+            array = array.OrderBy(x => x).ToArray();
+        }
+
+        private static void PrintArray()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(i + ". ->" + array[i]);
+            }
+        }
+
+        private static void ReadFromKeyboard()
+        {
+            int size = Reading.ReadInt("Zadej velikost pole");
+            array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = Reading.ReadInt("Zadej prvek");
+            }
+        }
+    }
+}
