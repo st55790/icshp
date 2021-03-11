70f7b60224e6578f6b0a2f0d3fed677967117b3f
Tomas Prudky <64365934+st55790@users.noreply.github.com>
2021-03-11 09:36:18 +0100

    add out parameters x1,x2

--- Exercises02/BaseLib/BaseLib/ExtraMath.cs
+++ Exercises02/BaseLib/BaseLib/ExtraMath.cs
@@ -15,17 +15,26 @@ namespace Fei.BaseLib
         /// <param name="a">The first term in the equation (x^2)</param>
         /// <param name="b">The second term in the equation (x)</param>
         /// <param name="c">The third term in the equation (number)</param>
-        /// <returns>true or false</returns>
-        public static bool QuadraticEquation(double a, double b, double c)
+        /// <param name="x1">root 1</param>
+        /// <param name="x2">root 2</param>
+        /// <returns>true or false and results int x1 a x2</returns>
+        public static bool QuadraticEquation(double a, double b, double c, out double x1, out double x2)
         {
             if ((Math.Pow(b, 2) - 4 * a * c) == 0)
             {
+                x1 = -b / 2 * a;
+                x2 = x1;
                 return true;
             }
             else if ((Math.Pow(b, 2) - 4 * a * c) > 0)
             {
+                double diskriminant = Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+                x1 = (-b + diskriminant) / 2 * a;
+                x2 = (-b - diskriminant) / 2 * a;
                 return true;
             }
+            x1 = double.NaN;
+            x2 = x1;
             return false;
         }
 
